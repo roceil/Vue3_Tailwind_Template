0846f53a18663e2c449c55710cbae0c18ba09efd
roceil <a0978006326@gmail.com>
2024-02-17 16:45:22 +0800

    feat(*): init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main class="w-screen h-screen flex justify-center items-center">
+    <h1 class="font-bold text-7xl text-gradient">Vue3 專案建立成功 !!!</h1>
+  </main>
+</template>
